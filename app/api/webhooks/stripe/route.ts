3eb1b8dea50cc5a573a05d4cd1bbb49dc0871276
Pablo Jordán <pablo.jordan.dlf@gmail.com>
2025-04-06 14:17:43 +0200

    stripe

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -2,7 +2,7 @@
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_API_KEY!, {
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: "2025-02-24.acacia",
 });
 
